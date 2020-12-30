6533c32730fe121ab187dd9b65dda1866ce26f98
Mert ÖZ <osemrt@gmail.com>
2020-12-31 02:16:26 +0300

    Configure redux store

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import DashboardPage from './components/DashboardPage';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 const jsx = (
-    <h1>Hello world!</h1>
+    <Provider store={store}>
+        <DashboardPage />
+    </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import productsReducer from '../reducers/products';
+import filtersReducer from '../reducers/filters';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            products: productsReducer,
+            filters: filtersReducer
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+    );
+
+    return store;
+}
