6cfa627930acaf22e5e424b676e3ce3302fa29e0
Mert ÖZ <osemrt@gmail.com>
2021-01-03 02:25:15 +0300

    Update the discounted products scrapper

--- src/scripts/discounted_products_scraping.py
+++ src/scripts/discounted_products_scraping.py
@@ -1,10 +1,14 @@
+import sys 
+import json
 import pandas as pd
+from selenium import webdriver
 
 class DiscountedProductScrapper():
     def __init__(self, driver):
         self.driver = driver
         self.gittigidiyor_url = 'https://www.gittigidiyor.com/yildiz-firsatlar'
         self.n11_url = 'https://www.n11.com/super-firsatlar'
+        self.products = []
 
 
     def set_page_urls(self,gittigidiyor_url, n11_url):
@@ -14,23 +18,23 @@ class DiscountedProductScrapper():
 
     def scrape(self):
 
-        products = self.gg_yildiz_firsatlar(self.gittigidiyor_url)
-        products_df = self.get_n11_discounted_products(self.n11_url)
+        self.__gg_yildiz_firsatlar(self.gittigidiyor_url)
+        self.__get_n11_discounted_products(self.n11_url)
 
-        return pd.concat([products,products_df])
+        return self.products
 
-    def gg_yildiz_firsatlar(self,link):
-    
-        products = list()
+    def __gg_yildiz_firsatlar(self,link):
+        
         self.driver.get(link)
 
-        product_links = self.driver.find_element_by_class_name("catalog-view")
+        catalog_view = self.driver.find_element_by_class_name("catalog-view")
         columns = self.driver.find_elements_by_class_name("product-info-con")
-
+        product_links = driver.find_elements_by_class_name("product-link")
+        
         for i,column in enumerate(columns):
             try:
 
-                prod_link = product_links.find_element_by_xpath('li[{}]/a'.format(i+1)).get_attribute('href')
+                product_link = catalog_view.find_element_by_xpath('li[{}]/a'.format(i+1)).get_attribute('href')
                 #print(prod_link)
 
                 aProduct = column.find_element_by_class_name("product-title")
@@ -39,16 +43,22 @@ class DiscountedProductScrapper():
 
                 price = column.find_element_by_class_name('fiyat')
                 #print(price.text)
-
-                products.append([prod_link,title.text,price.text,'GittiGidiyor'])
+                
+                img = product_links[i].find_element_by_class_name("img-cont")
+                image_url = img.get_attribute('data-original')
+                
+                self.products.append({
+                    'hostname': 'gittigidiyor.com',
+                    'productName': title.text,
+                    'currentPrice': self.format_price(price.text),
+                    'imageURL': image_url,
+                    'productURL': product_link
+                })
 
             except:
                 pass
-        products_df = pd.DataFrame(products,columns=['product_link','product_name','product_price','website'])
 
-        return products_df
-
-    def get_n11_discounted_products(self,link):
+    def __get_n11_discounted_products(self,link):
         
         products = list()
 
@@ -61,14 +71,44 @@ class DiscountedProductScrapper():
                 link = ss.get_attribute("href")
                 title = ss.get_attribute("title")
                 price = proDetail.find_element_by_tag_name('ins')
-                '''
-                print(link)
-                print(title)
-                print(price)
-                '''
-                products.append([link,title,price.text, 'N11'])
+                img = column.find_element_by_class_name("lazy")
+                img_URL = img.get_attribute('data-original')
+                
+                self.products.append({
+                    'hostname': 'n11.com',
+                    'productName': title,
+                    'currentPrice': self.format_price(price.text),
+                    'imageURL': img_URL,
+                    'productURL': link
+                })     
+
             except:
                 pass
+            
+    def format_price(self, price_string):
+        price_string = price_string.replace(' TL', '')
+        price_string = price_string.replace('.', '')
+        price_string = price_string.replace(',', '.')
+        price = float(price_string)
+        price = '{:.2f}'.format(price)
+        return price
 
-        products_df = pd.DataFrame(products,columns=['product_link','product_name','product_price','website'])
-        return products_df
+driver_path="./src/scripts/chromedriver.exe"
+
+if __name__ == '__main__':
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome(executable_path=driver_path, chrome_options=options)
+    
+    # Scrape discounted products
+    discountedProductScrapper = DiscountedProductScrapper(driver)
+    products = discountedProductScrapper.scrape()
+    
+    # Convert the dictionary array to JSON
+    result = json.dumps(products)
+    
+    # Send the result
+    print(result)
+    sys.stdout.flush()
