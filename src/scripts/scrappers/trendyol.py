ac5c3903a274712cf8c1caee0d4f7aad3c16889e
Caner Kaya <canerkaya718@gmail.com>
2021-01-05 21:42:40 +0300

    Update trendyol.py
    scrapped product img

--- src/scripts/scrappers/trendyol.py
+++ src/scripts/scrappers/trendyol.py
@@ -2,18 +2,34 @@ class TrendyolScrapper:
     def __init__(self, driver):
         self.driver = driver
         self.url = None
-        
+
     def scrape(self, url):
         # Go to the given product
-        self.driver.get(url)   
+        self.driver.get(url)
+        # Scrape product image
+        img_url = self.scrape_product_image()
+        # Scrape product name
+        product_name = self.scrape_product_name()
+        # Scrape product price
+        price = self.scrape_price()
+        return img_url, product_name, price
+
+    def scrape_product_image(self):
+        # Find the element
+        img_obj = self.driver.find_element_by_class_name('slick-list')
+        img_url = img_obj.find_element_by_tag_name('img').get_attribute('src')
+        return img_url
+
+    def scrape_price(self):
         try:
-            return self.scrape_product_name(),self.scrape_prc_slg()
+            return self.scrape_prc_slg()
         except Exception:
             pass
         try:
-            return self.scrape_product_name(),self.scrape_prc_dsc()
+            return self.scrape_prc_dsc()
         except Exception:
-            return self.scrape_product_name(), 0.0
+            return 0.0
+
         
     def scrape_prc_slg(self):
         # Find the element
